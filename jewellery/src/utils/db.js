26fab78c3cd64cfc71e598ee8ef4f4d1980869a5
GOKUL NR <118470070+gokulnr02@users.noreply.github.com>
2025-04-03 23:42:15 +0530

    validation2

--- jewellery/src/utils/db.js
+++ jewellery/src/utils/db.js
@@ -1,18 +1,5 @@
 import sql from "mssql";
 
-// const config = {
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   server: process.env.DB_SERVER, // Example: "localhost"
-//   database: process.env.DB_NAME,
-//   port: 1433,
-//   options: {
-//     encrypt: false, // Required for Azure
-//     trustServerCertificate: true, 
-//     connectionTimeout: 30000,// Required for local development
-//   },
-// };
-// console.log(config,"13")
 
 const config = 'Server=sql5113.site4now.net,1433;Database=db_9e515d_latha;User Id=db_9e515d_latha_admin;Password=Password123;Encrypt=false;TrustServerCertificate=true;';
 let dbPool;
@@ -25,7 +12,6 @@ export async function connectDB() {
     }
     return dbPool;
   } catch (error) {
-    // console.error("❌ Database connection error:", error);
-    throw new Error("Database connection failed");
+   console.log(error,'Db connection error')
   }
 }
